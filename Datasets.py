db5f7fe6a40a5fcbde0956f0338cabf4d93da9d3
Tiago Antao <tiago@tiago.org>
2021-10-28 17:13:38 -0400

    Chapter 1 datasets

---
+++ Datasets.py
@@ -0,0 +1,18 @@
+# # Datasets for the book
+#
+# Here we provide links to the datasets used in the book.
+#
+# Important Notes:
+#
+# 1. Note that these datasets are provided on external servers by third parties
+
+# # Python and the Surrounding Software Ecology
+#
+# ## Interfacing with R via rpy2
+#
+#     sequence.index Please FTP from this URL(cut and paste)
+#
+# ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/historical_data/former_toplevel/sequence.index
+#
+
+
